623d5206a8ad34bfa92fc821b28cec728ab0d73e
Maksym Rudnyi <mrudnyi@idiq.com>
2023-02-23 21:02:10 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+  //options here
+  autoScrolling:true,
+  scrollHorizontally: true,
+  sectionsColor : ['#fe5f46', '#0998ec', '#fc6c7c', '#fec304'],
+  navigation: true,
+  slidesNavigation: true,
+  anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
+  navigationTooltips: ['first', 'second']
+});
